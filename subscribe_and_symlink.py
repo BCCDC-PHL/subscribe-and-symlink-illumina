7e03af256ca600a3d00162f720f084e3e36f3386
Dan Fornika <dfornika@gmail.com>
2020-10-07 17:11:39 -0700

    Make keypair optional

--- subscribe_and_symlink.py
+++ subscribe_and_symlink.py
@@ -26,13 +26,15 @@ def main(args):
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
 
-    client_secret_file = args.private_key
-    client_public, client_secret = zmq.auth.load_certificate(client_secret_file)
-    socket.curve_secretkey = client_secret
-    socket.curve_publickey = client_public
-    server_public_file = args.public_key
-    server_public, _ = zmq.auth.load_certificate(server_public_file)
-    socket.curve_serverkey = server_public
+    if args.private_key and args.public_key:
+        client_secret_file = args.private_key
+        client_public, client_secret = zmq.auth.load_certificate(client_secret_file)
+        socket.curve_secretkey = client_secret
+        socket.curve_publickey = client_public
+        server_public_file = args.public_key
+        server_public, _ = zmq.auth.load_certificate(server_public_file)
+        socket.curve_serverkey = server_public
+
     print("Collecting updates from server...")
     socket.connect("tcp://127.0.0.1:%s" % args.port)
 
@@ -66,7 +68,7 @@ if __name__ == '__main__':
     parser.add_argument('--topic', default="illumina_runs")
     parser.add_argument('--experiment_name_regex', default=".+")
     parser.add_argument('--symlink_directory', default=".")
-    parser.add_argument('--public_key', required=True)
-    parser.add_argument('--private_key', required=True)
+    parser.add_argument('--public_key')
+    parser.add_argument('--private_key')
     args = parser.parse_args()
     main(args)
